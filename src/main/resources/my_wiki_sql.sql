673f0c320779ad2ef12cd1a9942345e5fe9708a3
Peng Liu <pliu@lattice-engines.com>
2023-05-30 13:08:21 +0800

    更新mysql脚本

--- src/main/resources/my_wiki_sql.sql
+++ src/main/resources/my_wiki_sql.sql
@@ -8,15 +8,17 @@ CREATE TABLE `wiki_category` (
   `created_datetime` datetime NOT NULL,
   `updated_datetime` datetime NOT NULL,
   `is_default` tinyint(1) NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) NOT NULL DEFAULT '0',
+  `is_blog` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `wiki_image` (
   `id` int NOT NULL AUTO_INCREMENT,
   `file_name` varchar(255) NOT NULL,
   `upload_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `wiki` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -30,7 +32,7 @@ CREATE TABLE `wiki` (
   PRIMARY KEY (`id`),
   KEY `category_type_id_idx` (`category_type_id`),
   CONSTRAINT `category_type_id` FOREIGN KEY (`category_type_id`) REFERENCES `wiki_category` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `nav_node` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -45,4 +47,4 @@ CREATE TABLE `nav_node` (
   KEY `nav_node_wiki_category` (`category_type_id`),
   CONSTRAINT `nav_node_wiki_category` FOREIGN KEY (`category_type_id`) REFERENCES `wiki_category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `parent_id` FOREIGN KEY (`parent_id`) REFERENCES `nav_node` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=584 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=601 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
